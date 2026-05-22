45c6d08a1819ae53a483dccbb227c931b012a04c
SidneiEwerton <151098486+SidneiEwerton@users.noreply.github.com>
2026-05-21 22:26:30 -0300

    Delete lib directory

--- lib/core/storage/preferences_service_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:busca_clima2/core/storage/preferences_service.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'preferences_service_provider.g.dart';
-
-@riverpod
-PreferencesService preferencesService(Ref ref) {
-  return PreferencesService();
-}
