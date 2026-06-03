6aa7aab46d61d8863ccfdf3e687732f2afb28588
SidneiEwerton <151098486+SidneiEwerton@users.noreply.github.com>
2026-06-02 22:13:13 -0300

    Delete lib directory

--- lib/core/constants/api_constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:busca_clima2/core/env/env.dart';
-
-class ApiConstants {
-
-  static String get apiKey => Env.apiKey;
-  static const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
-  
-}
--- lib/features/weather/data/repositories/geocoding_repository.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:busca_clima2/features/weather/domain/models/geocoding_model.dart';
-@override
-abstract class GeocodingRepository {
-  Future<List<GeocodingModel>> fetchCityOptions(String name);
-}
