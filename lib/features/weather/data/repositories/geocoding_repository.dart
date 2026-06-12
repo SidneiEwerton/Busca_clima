415e6aa35cfdbbe57d2da1588b297df814ceb5a7
Leandro <l.silva01@uol.com.br>
2026-06-13 00:01:20 +0100

    Removido o @override inválido que estava antes da declaração abstract class.

--- lib/features/weather/data/repositories/geocoding_repository.dart
+++ lib/features/weather/data/repositories/geocoding_repository.dart
@@ -1,7 +1,5 @@
-
-
 import 'package:busca_clima2/features/weather/domain/models/geocoding_model.dart';
-@override
+
 abstract class GeocodingRepository {
   Future<List<GeocodingModel>> fetchCityOptions(String name);
 }
